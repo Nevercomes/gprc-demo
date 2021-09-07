9e06af2fdf73b91c2502205c7aa9223c2e8992e4
Jiacheng Sun <jiachengs@moqi.ai>
2021-09-07 16:44:32 +0800

    finish service

--- src/main/java/com/example/grpcdemo/service/HelloWorldService.java
+++ src/main/java/com/example/grpcdemo/service/HelloWorldService.java
@@ -1,4 +1,23 @@
 package com.example.grpcdemo.service;
 
-public class HelloWorldService {
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import com.example.grpc.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class HelloWorldService extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String greeting = "Hello, " +
+                request.getFirstName() +
+                " " +
+                request.getLastName();
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
