75c829553f65f8082ddf782ae8560f5f527e2eed
Jiacheng Sun <jiachengs@moqi.ai>
2021-09-07 16:50:38 +0800

    start grpc server

--- src/main/java/com/example/grpcdemo/GrpcDemoApplication.java
+++ src/main/java/com/example/grpcdemo/GrpcDemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.grpcdemo;
 
+import com.example.grpcdemo.service.HelloWorldService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GrpcDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GrpcDemoApplication.class, args);
+        // Run the grpc server
+        Server server = ServerBuilder
+                .forPort(9000)
+                .addService(new HelloWorldService()).build();
+
+        try {
+            server.start();
+            server.awaitTermination(); // block server
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
